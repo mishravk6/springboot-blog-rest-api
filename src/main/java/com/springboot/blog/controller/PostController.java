7f4c6e8c6eb280fca3f9370e5dc90a1acca05670
Vineet Mishra <vineet.mishra.bah@gmail.com>
2022-03-10 00:12:35 +0530

    Initial commit

---
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -0,0 +1,54 @@
+package com.springboot.blog.controller;
+
+import com.springboot.blog.dto.PostDto;
+import com.springboot.blog.entity.Post;
+import com.springboot.blog.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private PostService postService;
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    //create blog post
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
+    //getAll post rest api
+    @GetMapping
+    public List<PostDto> getAllPosts(){
+        return postService.getAllPost();
+    }
+
+    //get post by id
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name="id") long id){
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+    // update post by rest api
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable(name="id") long id){
+
+        //PostDto postResponse = postService.updatePost(postDto,id);
+
+        return new ResponseEntity<>(postService.updatePost(postDto,id),HttpStatus.OK);
+    }
+
+    //delete post rest api
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable(name="id") long id){
+        postService.deletePostById(id);
+        return new ResponseEntity<>("Post entity deleted successfully.",HttpStatus.OK);
+    }
+}
